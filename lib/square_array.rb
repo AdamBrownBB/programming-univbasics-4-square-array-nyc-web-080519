4fc7a282a7b272318cbf0e8724eb3252a965661d
Adam Brown <adam.brown.betty@gmail.com>
2019-07-31 23:40:59 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+def square_array(numbers)
+  new_numbers = []
+  counter = 0
+ 
+  while counter < new_numbers.length do
+     numbers.push(numbers **2) 
+    counter += 1
+  end
+  return new_numbers
 end
+
